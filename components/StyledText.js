ec518b402a5bdddd5c98c78026034d1d5af1c409
Alvin_Zou <alvinzou@college.harvard.edu>
2020-06-06 17:33:59 -0700

    Starting work on home screen

--- components/StyledText.js
+++ components/StyledText.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { theme } from '../theme.js';
+import theme from '../theme.js';
 
-export function MonoText(props) {
-  return <Text {...props} style={[props.style, { fontFamily: 'Rockwell' }]} />;
+export function StyledText(props) {
+  return <Text {...props} style={[props.style, { fontFamily: theme.fonts.titles }]} />;
 }
--- theme.js
+++ theme.js
@@ -4,15 +4,17 @@ const theme = {
     colors: {
         primary: {
             safe: '#84C1CC',
-            warning: '',
-            limited: '',
-            quarantined: '',
+            limited: '#AA7372',
+            restricted: '#6C1915',
+            quarantined: '#455624',
             notification: '', // red - number of notifications
+            background: '#FFFFFF',
+            contactsGraph: '#C2CEAA',
         },
         fonts: {
-            light: '',
-            medium: '',
-            dark: '#000000',
+            light: '#E2E2E2',
+            medium: '#707070',
+            dark: '#4D4D4D',
         },
     },
     fonts: {
