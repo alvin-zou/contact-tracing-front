ca5692cbae71a40694d40fec56a650183f605645
Alvin_Zou <alvinzou@college.harvard.edu>
2020-06-30 01:03:41 -0700

    Started StatsScreen, figured out how to do circles

--- screens/StatsScreen.js
+++ screens/StatsScreen.js
@@ -1,74 +1,146 @@
-import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import theme from '../theme.js';
+import PieChart from 'react-native-pie-chart';
+import { Dimensions, TouchableHighlight } from 'react-native';
+import { MonoText } from '../components/StyledText';
+
+const status = "safe"; // safe, limited, restricted, quarantined
+
+statusColor = (status==="safe" ? theme.colors.primary.safe : 
+(status==="limited" ? theme.colors.primary.limited : 
+(status==="restricted" ? theme.colors.primary.restricted : 
+(status==="quarantined" ? theme.colors.primary.quarantined : 'black'))));
 
 export default function StatsScreen() {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
 
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-      />
+        <View style={styles.getStartedContainer}>
 
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-        isLastOption
-      />
-    </ScrollView>
+          <Text style={styles.titleText}>
+            stats
+          </Text>
+          <Text style={styles.subText}>
+            TODAY'S CONTACTS
+          </Text>
+          <TouchableHighlight
+      style = {{
+        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+        width: Dimensions.get('window').width * 0.5,
+        height: Dimensions.get('window').width * 0.5,
+        backgroundColor:'#f00',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      underlayColor = '#ccc'
+      onPress = { () => alert('Yaay!') }
+    >
+      <Text> Mom, look, I am a circle! </Text>
+    </TouchableHighlight>
+          <Text style={styles.subText}>
+            WEEKLY STATISTICS
+          </Text>
+          <Text style={styles.subText}>
+            CUMULATIVE SCORE
+          </Text>
+
+        </View>
+
+      </ScrollView>
+
+    </View>
   );
 }
 
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
+StatsScreen.navigationOptions = {
+  header: null,
+};
+
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
+}
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: 'white',
   },
   contentContainer: {
-    paddingTop: 15,
+    paddingTop: 30,
   },
-  optionIconContainer: {
-    marginRight: 12,
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
   },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: status==="quarantined" ? 23 : 15,
   },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  imageContainer: {
+    alignItems: 'center',
+    paddingTop: '0%',
+    width: '10%',
+    height: '10%',
+    color: theme.colors.primary.background,
   },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
+  image: {
+    flex: 1,
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+  },
+  titleText: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    paddingTop: status==="quarantined" ? '58%' : '55%',
+    color: statusColor,
+    fontFamily: theme.fonts.titles,
+    textAlign: 'center',
+    letterSpacing: 3,
+  },
+  subText: {
+    fontSize: 26,
+    paddingTop: (status==="safe")||(status==="restricted") ? '15%' : '5%',
+    color: theme.colors.fonts.dark,
+    fontFamily: theme.fonts.secondary,
+    textAlign: 'center',
+    letterSpacing: 3,
+    marginHorizontal: status==="restricted" ? 10 : 8,
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
   },
 });
