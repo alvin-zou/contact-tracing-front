c0bf72489cf0971ab2c0b5cdf823d525d5ae8f09
kian2attari <kian2attari@yahoo.com>
2020-08-08 21:19:23 -0400

    added prettier config

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import * as fire from "./Fire.js";
+import * as fire from './Fire.js';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
 
 import WelcomeScreen from './screens/WelcomeScreen';
-import OneMessageScreen from './screens/OneMessageScreen'; 
+import OneMessageScreen from './screens/OneMessageScreen';
 
 const Stack = createStackNavigator();
 
@@ -21,16 +21,18 @@ export default function App(props) {
   } else {
     return (
       fire.init(),
-      <View style={styles.container}>
-        {/* {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}*/}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator initialRouteName="Welcome">
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="OneMessage" component={OneMessageScreen}  />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
+      (
+        <View style={styles.container}>
+          {/* {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}*/}
+          <NavigationContainer linking={LinkingConfiguration}>
+            <Stack.Navigator initialRouteName='Welcome'>
+              <Stack.Screen name='Root' component={BottomTabNavigator} />
+              <Stack.Screen name='Welcome' component={WelcomeScreen} />
+              <Stack.Screen name='OneMessage' component={OneMessageScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </View>
+      )
     );
   }
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,22 @@
 // import * as WebBrowser from 'expo-web-browser';
 import { Ionicons } from '@expo/vector-icons';
-import React, {useState} from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import theme from '../theme.js';
 // import { MonoText } from '../components/StyledText';
 
-let status = "safe"; // safe, limited, restricted, quarantined
+let status = 'safe'; // safe, limited, restricted, quarantined
 
 export default function HomeScreen() {
-
   // const [showInfo, setShowInfo] = useState(false);
 
   // if (status === "safe") {
@@ -18,89 +25,98 @@ export default function HomeScreen() {
 
   const infoAlert = () => {
     Alert.alert(
-      "Safe Screen",
+      'Safe Screen',
       "A safe screen means that you've interacted with few (if any) infectious people since your last diagonistic test. So we're not asking you to quarantine. As always, continue to follow our social distancing guidelines, and let us know if you feel unwell.",
-      [
-        {text: "CLOSE", style:"cancel"}
-      ]
-    )}
-    
-  let info, reportSymptoms; 
+      [{ text: 'CLOSE', style: 'cancel' }]
+    );
+  };
 
-  if (status === "safe") {
-    info = <TouchableOpacity style={styles.info}  onPress={infoAlert}>
-    <Ionicons
-      name={"ios-information-circle"}
-      size={30}
-      color={"white"}
-    />
-    <Text style={styles.infoText}>
-          MORE INFO
-    </Text>
-  </TouchableOpacity>
+  let info, reportSymptoms;
 
-   
+  if (status === 'safe') {
+    info = (
+      <TouchableOpacity style={styles.info} onPress={infoAlert}>
+        <Ionicons name={'ios-information-circle'} size={30} color={'white'} />
+        <Text style={styles.infoText}>MORE INFO</Text>
+      </TouchableOpacity>
+    );
   }
-  reportSymptoms = <TouchableOpacity>
-    <View  style={styles.report}>
+  reportSymptoms = (
+    <TouchableOpacity>
+      <View style={styles.report}>
+        <Ionicons
+          name={'md-add-circle'}
+          size={30}
+          color={'red'}
+          style={{ right: 10 }}
+        />
+        <Text style={styles.reportText}>REPORT SYMPTOMS</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
-    <Ionicons
-    name={"md-add-circle"}
-    size={30}
-    color={"red"}
-    style={{right: 10}}
-  />
-  <Text style={styles.reportText}>
-        REPORT SYMPTOMS
-  </Text>
-    
-    </View>
-  </TouchableOpacity>
-  
-  
   return (
     <View style={styles.container}>
-
-      
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
         {info}
 
         <View style={styles.getStartedContainer}>
-
+          {/* TODO use a switch statement instead of these nested ternary operators*/}
           <View style={styles.imageContainer}>
-            <Image style={styles.image} source={require(
-              status==="safe" ? '../assets/images/Safe.png' : 
-              (status==="limited" ? '../assets/images/LimitContact.png' : 
-              (status==="restricted" ? '../assets/images/StayHome.png' : 
-              (status==="quarantined" ? '../assets/images/Quarantine.png' : 'black'))))} />
+            <Image
+              style={styles.image}
+              source={require(status === 'safe'
+                ? '../assets/images/Safe.png'
+                : status === 'limited'
+                ? '../assets/images/LimitContact.png'
+                : status === 'restricted'
+                ? '../assets/images/StayHome.png'
+                : status === 'quarantined'
+                ? '../assets/images/Quarantine.png'
+                : 'black')}
+            />
           </View>
 
           <Text style={styles.titleText}>
-            {status==="safe" ? `safe` : 
-              (status==="limited" ? `caution` : 
-              (status==="restricted" ? `exposed` : 
-              (status==="quarantined" ? `infected` : 'black')))}
+            {status === 'safe'
+              ? `safe`
+              : status === 'limited'
+              ? `caution`
+              : status === 'restricted'
+              ? `exposed`
+              : status === 'quarantined'
+              ? `infected`
+              : 'black'}
           </Text>
           <Text style={styles.subText}>
-            {status==="safe" ? `YOU'RE SHOWING LOW LEVELS OF EXPOSURE TO COVID-19.` : 
-              (status==="limited" ? `YOU'RE SHOWING ELEVATED LEVELS OF POSSIBLE COVID-19 EXPOSURE.` : 
-              (status==="restricted" ? `YOU'VE COME INTO CONTACT WITH SOMEONE WHO HAS COVID-19.` : 
-              (status==="quarantined" ? `YOU'VE BEEN INFECTED.` : 'black')))}
+            {status === 'safe'
+              ? `YOU'RE SHOWING LOW LEVELS OF EXPOSURE TO COVID-19.`
+              : status === 'limited'
+              ? `YOU'RE SHOWING ELEVATED LEVELS OF POSSIBLE COVID-19 EXPOSURE.`
+              : status === 'restricted'
+              ? `YOU'VE COME INTO CONTACT WITH SOMEONE WHO HAS COVID-19.`
+              : status === 'quarantined'
+              ? `YOU'VE BEEN INFECTED.`
+              : 'black'}
           </Text>
           <Text style={styles.subText}>
-            {status==="safe" ? `THANKS FOR KEEPING HARVARD HEALTHY.` : 
-              (status==="limited" ? `LIMIT CONTACT WHEN POSSIBLE UNTIL YOUR NEXT NEGATIVE TEST.` : 
-              (status==="restricted" ? `QUARANTINE IN YOUR ROOM UNTIL YOUR NEXT NEGATIVE TEST.` : 
-              (status==="quarantined" ? `PLEASE SELF-ISOLATE UNTIL YOUR NEXT NEGATIVE TEST. IF SYMPTOMS WORSEN, SEEK MEDICAL ATTENTION.` : 'black')))}
+            {status === 'safe'
+              ? `THANKS FOR KEEPING HARVARD HEALTHY.`
+              : status === 'limited'
+              ? `LIMIT CONTACT WHEN POSSIBLE UNTIL YOUR NEXT NEGATIVE TEST.`
+              : status === 'restricted'
+              ? `QUARANTINE IN YOUR ROOM UNTIL YOUR NEXT NEGATIVE TEST.`
+              : status === 'quarantined'
+              ? `PLEASE SELF-ISOLATE UNTIL YOUR NEXT NEGATIVE TEST. IF SYMPTOMS WORSEN, SEEK MEDICAL ATTENTION.`
+              : 'black'}
           </Text>
-
         </View>
 
         {reportSymptoms}
-
       </ScrollView>
-
     </View>
   );
 }
@@ -122,10 +138,16 @@ HomeScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: status==="safe" ? theme.colors.primary.safe : 
-      (status==="limited" ? theme.colors.primary.limited : 
-      (status==="restricted" ? theme.colors.primary.restricted : 
-      (status==="quarantined" ? theme.colors.primary.quarantined : 'black'))),
+    backgroundColor:
+      status === 'safe'
+        ? theme.colors.primary.safe
+        : status === 'limited'
+        ? theme.colors.primary.limited
+        : status === 'restricted'
+        ? theme.colors.primary.restricted
+        : status === 'quarantined'
+        ? theme.colors.primary.quarantined
+        : 'black',
   },
   contentContainer: {
     paddingTop: 30,
@@ -137,20 +159,19 @@ const styles = StyleSheet.create({
   },
   getStartedContainer: {
     alignItems: 'center',
-    marginHorizontal: status==="quarantined" ? 23 : 15,
+    marginHorizontal: status === 'quarantined' ? 23 : 15,
   },
   imageContainer: {
     alignItems: 'center',
     justifyContent: 'center',
     width: 250,
     height: 250,
-    backgroundColor: "white",
-    borderColor: "white",
+    backgroundColor: 'white',
+    borderColor: 'white',
     borderWidth: 5,
     borderRadius: 250 / 2,
   },
-  image: {
-  },
+  image: {},
   titleText: {
     fontSize: 60,
     fontWeight: 'bold',
@@ -162,41 +183,41 @@ const styles = StyleSheet.create({
   },
   subText: {
     fontSize: 26,
-    paddingTop: (status==="safe")||(status==="restricted") ? '15%' : '5%',
+    paddingTop: status === 'safe' || status === 'restricted' ? '15%' : '5%',
     color: theme.colors.primary.background,
     fontFamily: theme.fonts.secondary,
     textAlign: 'center',
     letterSpacing: 3,
-    marginHorizontal: status==="restricted" ? 10 : 8,
+    marginHorizontal: status === 'restricted' ? 10 : 8,
   },
   infoText: {
     fontSize: 20,
     color: theme.colors.primary.background,
     fontFamily: theme.fonts.secondary,
     letterSpacing: 1,
-    textDecorationLine: "underline",
+    textDecorationLine: 'underline',
     left: 10,
     top: 1,
   },
   info: {
     bottom: 20,
     left: 10,
-    flexDirection: "row",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
   },
 
   report: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
     marginTop: 35,
     flex: 1,
     height: 50,
   },
   reportText: {
     fontSize: 20,
-    color: "red",
+    color: 'red',
     fontFamily: theme.fonts.secondary,
     letterSpacing: 1,
   },
