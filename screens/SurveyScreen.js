b535f79fb348f32406957816ec46dd95448a5d57
mosesmayer <mosesmayer@gmail.com>
2020-08-25 07:34:04 +0700

    Survey Screen according to UI design - test again for functionality

--- screens/SurveyScreen.js
+++ screens/SurveyScreen.js
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import {  TextInput, View, Button, StyleSheet } from 'react-native';
+import { TextInput, View, Button, StyleSheet } from 'react-native';
 import theme from '../theme.js';
 import * as fire from "../Fire.js";
 import { uid } from '../App.js';
 
 export default class SurveyScreen extends React.Component {
   state = {
-    firstName: '', lastName: '', house: ''
+    firstName: '', lastName: '', house: '',
+    email: '', password: '', confirmpw: '',
+    surveyPartTwo: false,
   }
 
-  render() {
+  SurveyPartOneFrag = () => {
     return (
-      <View style={styles.container}>
+      <View>
         <TextInput
           placeholder="First name"
           onChangeText={(text) => this.setState({ firstName: text })}
@@ -24,16 +26,66 @@ export default class SurveyScreen extends React.Component {
           placeholder="House"
           onChangeText={(text) => this.setState({ house: text })}
         />
+      </View>
+    )
+  }
+
+  SurveyPartTwoFrag = () => {
+    return (
+      <View>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(text) => this.setState({ email: text })}
+        />
+        <TextInput
+          placeholder="Password"
+          textContentType="password"
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <TextInput
+          placeholder="Confirm Password"
+          textContentType="password"
+          onChangeText={(text) => this.setState({ confirm: text })}
+        />
+      </View>
+    )
+  }
+
+  confirmFirstPart = () =>
+    Alert.alert(
+      "Are the details entered correct?",
+      "Press OK to confirm entered details",
+      [
+        {
+          text: "Cancel",
+          onPress: () => { },
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => this.setState({ surveyPartTwo: true }) }
+      ],
+      { cancelable: false }
+    );
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.surveyPartTwo ? this.SurveyPartTwoFrag() : this.SurveyPartOneFrag()}
         <Button
-        title='Submit'
-        onPress={() => { fire.writeUserData(
-                  uid,
-                  this.state.firstName,
-                  this.state.lastName,
-                  this.state.house);
-                  this.props.navigation.navigate('Root'); }}>
+          title='Submit'
+          onPress={() => {
+            if (!this.state.surveyPartTwo) {
+              this.confirmFirstPart();
+              return;
+            }
+            fire.writeUserData(
+              uid,
+              this.state.firstName,
+              this.state.lastName,
+              this.state.house);
+            this.props.navigation.navigate('Root');
+          }}>
         </Button>
-    </View>
+      </View>
     );
   }
 }
